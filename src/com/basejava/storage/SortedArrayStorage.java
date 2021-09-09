6b575d580f8debfd2cabd36e300e898240c818fd
AlenaS22 <alena_w93@mail.ru>
2021-09-09 15:35:35 +0300

    HW_5_2 (refactor_2)

--- src/com/basejava/storage/SortedArrayStorage.java
+++ src/com/basejava/storage/SortedArrayStorage.java
@@ -5,14 +5,12 @@ import com.basejava.model.Resume;
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-    @Override
     protected void saveToArray(Resume resume, int index) {
         index = Math.abs(index + 1);
         System.arraycopy(storage, index, storage, index + 1, size - index);
         storage[index] = resume;
     }
 
-    @Override
     protected Object getSearchKey(String uuid) {
         Resume searchKey = new Resume(uuid);
         // Arrays.binarySearch возвращает позицию заданного значения
